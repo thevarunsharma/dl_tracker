ec9054cc7e40d590c730837e26ed27aed252e045
thevarunsharma <varunsharma123.del@gmail.com>
2020-03-24 01:28:17 +0530

    read, fetch data from database

---
+++ dbaccess.py
@@ -0,0 +1,47 @@
+from model_tracker.auth import authenticate_database
+import requests
+
+class DBAccessHandle:
+
+    def __init__(self, model_key, password):
+        self.__username = authenticate_database(model_key, password)
+        self.__model_key = model_key
+        self.__url = "http://127.0.0.1:5000/"
+
+    @staticmethod
+    def serialized(JSON):
+        JSON = JSON.copy()
+        for k in JSON:
+            JSON[k] = str(JSON[k])
+        return JSON
+
+    def start_training(self, JSON):
+        resp = requests.post(self.__url+"start_training", json={
+            "model_key" : self.__model_key,
+            "username" : self.__username,
+            "JSON" : self.serialized(JSON)
+        })
+
+        new_training_id = resp.json().get("new_training_id")
+        return new_training_id
+
+    def end_training(self, training_id):
+        resp = requests.post(self.__url+"end_training", json={
+            "model_key" : self.__model_key,
+            "username" : self.__username,
+            "training_id" : training_id
+        })
+
+    def epoch_begin(self, JSON):
+        resp = requests.post(self.__url+"epoch_begin", json={
+            "model_key" : self.__model_key,
+            "username" : self.__username,
+            "JSON" : self.serialized(JSON)
+        })
+
+    def epoch_end(self, JSON):
+        resp = requests.post(self.__url+"epoch_end", json={
+            "model_key" : self.__model_key,
+            "username" : self.__username,
+            "JSON" : self.serialized(JSON)
+        })
